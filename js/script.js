4f8e20e88b441ec0580bb824a01e9fba014b6197
sofiahernandezas <sofiahernandezastig@gmail.com>
2024-08-20 00:37:52 -0300

    Fetch
    Se realiza fetch al archivo con los datos y se muestra los estudiantes con la función showData

--- js/script.js
+++ js/script.js
@@ -19,3 +19,12 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch('json/data.json') 
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    dataArray = data.students;
+    showData(dataArray);
+
+  
+  });
